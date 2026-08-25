c6532aabddffa6b65f82c7b3a74caa912c2da91a
sciammag <205177512+sciammag@users.noreply.github.com>
2026-08-25 17:55:08 +1000

    Adjust syntax

--- scripts/04CDA.R
+++ scripts/04CDA.R
@@ -24,7 +24,7 @@ data <- read.csv(file.path("output", "dataset", "habitat_dataset.csv"))
 colnames(data)[colnames(data) == "Life.stage"] <- "Stage"
 data$TL <- as.numeric(data$TL)
 data$Species <- as.factor(data$Species)
-data$Stage<- as.factor(data$Stage)
+data$Stage <- as.factor(data$Stage)
 data$Site <- as.factor(data$Site)
 data <- within(data, rm(`Other..clam..sponges..holes.`, `Coarse.Branching`))
 data <- data |>
@@ -46,8 +46,10 @@ chrys <- droplevels(chrys)
 mol <- subset(data, Species == "moluccensis" | Species == "control")
 mol <- droplevels(mol)
 
-ambo <- within(ambo, rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site, Species))
-chrys <- within(chrys, rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site, Species))
+ambo <- within(ambo,
+     rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site, Species))
+chrys <- within(chrys,
+     rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site, Species))
 mol <- within(mol, rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site, Species))
 
 head(ambo)
@@ -64,75 +66,74 @@ describe(ambo[, 2:8])
 describeBy(ambo[, 2:8], ambo$Stage)
 pairs.panels(ambo[, 2:8])
 
-a.matrix <- as.matrix(ambo[, 2:8])
+a_matrix <- as.matrix(ambo[, 2:8])
 
-model.ambo <- manova(a.matrix ~ Stage, data = ambo)
+model_ambo <- manova(a_matrix ~ Stage, data = ambo)
 
-model.ambo
-summary(model.ambo)
+model_ambo
+summary(model_ambo)
 
-ambo.disc <- candisc(model.ambo, term = "Stage")
-ambo.disc
-summary(ambo.disc)
+ambo_disc <- candisc(model_ambo, term = "Stage")
+ambo_disc
+summary(ambo_disc)
 
-ambo.disc.2 <- candisc(model.ambo, data = ambo, ndim = 1)
-plot(ambo.disc.2, var.col = "black", col = palette,
-var.lwd = 1, ylim = c(-4, 5))
+ambo_disc2 <- candisc(model_ambo, data = ambo, ndim = 1)
+plot(ambo_disc2, var.col = "black", col = palette, var.lwd = 1, ylim = c(-4, 5))
 
-ambo.disc$structure
-ambo.disc$coeffs.std
-ambo.disc$coeffs.raw
+ambo_disc$structure
+ambo_disc$coeffs.std
+ambo_disc$coeffs.raw
 
 ## Plot----
-palette <- c("#227685","black","#689836", "#F2501D")
+palette <- c("#227685", "black", "#689836", "#F2501D")
 
-heplot(ambo.disc, col = palette, var.col = "black")
-plot(ambo.disc, conf = 0.95, ellipse = TRUE, ellipse.prob = 0.95, col = palette, 
-     var.col = "black", var.lwd = 2, pch = c(20,20,20,20))
-plot(ambo.disc, ellipse = FALSE, col = palette, var.col = "black", 
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(ambo.disc, conf = 0.95,
+heplot(ambo_disc, col = palette, var.col = "black")
+plot(ambo_disc, conf = 0.95, ellipse = TRUE, ellipse.prob = 0.95,
+     col = palette, var.col = "black",
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(ambo_disc, ellipse = FALSE, col = palette, var.col = "black",
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(ambo_disc, conf = 0.95,
      col = palette, var.col = "black",
      xlim = c(-1, 1), ylim = c(-1, 1),
-     var.lwd = 2, type = 'n')
+     var.lwd = 2, type = "n")
 
 # p chrysurus----
 describe(chrys[, 2:8])
 describeBy(chrys[, 2:8], chrys$Stage)
 pairs.panels(chrys[, 2:8])
 
-c.matrix <- as.matrix(chrys[, 2:8])
+c_matrix <- as.matrix(chrys[, 2:8])
 
-model.chrys <- manova(c.matrix ~ Stage, data = chrys)
+model_chrys <- manova(c_matrix ~ Stage, data = chrys)
 
-model.chrys
-summary(model.chrys)
+model_chrys
+summary(model_chrys)
 
-chrys.disc <- candisc(model.chrys, term = "Stage")
-chrys.disc
-summary(chrys.disc)
+chrys_disc <- candisc(model_chrys, term = "Stage")
+chrys_disc
+summary(chrys_disc)
 
-chrys.disc.2 <- candisc(model.chrys, data = chrys, ndim = 1)
-plot(chrys.disc.2, var.col = "black", col = palette,
-var.lwd = 1, ylim = c(-4, 5))
+chrys_disc2 <- candisc(model_chrys, data = chrys, ndim = 1)
+plot(chrys_disc2, var.col = "black", col = palette, var.lwd = 1, ylim = c(-4, 5))
 
-chrys.disc$structure
-chrys.disc$coeffs.std
-chrys.disc$coeffs.raw
+chrys_disc$structure
+chrys_disc$coeffs.std
+chrys_disc$coeffs.raw
 
 ## Plot----
-heplot(chrys.disc, col = palette, var.col = "black")
-plot(chrys.disc,
+heplot(chrys_disc, col = palette, var.col = "black")
+plot(chrys_disc,
      conf = 0.95, ellipse = TRUE, ellipse.prob = 0.95,
      col = palette, var.col = "black",
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(chrys.disc, ellipse = FALSE,
-     col = palette, var.col = "black", 
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(chrys.disc, conf = 0.95,
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(chrys_disc, ellipse = FALSE,
+     col = palette, var.col = "black",
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(chrys_disc, conf = 0.95,
      col = palette, var.col = "black",
      xlim = c(-1, 1), ylim = c(-1, 1),
-     var.lwd = 2, type = 'n')
+     var.lwd = 2, type = "n")
 
 # p moluccensis----
 describe(mol[, 2:8])
@@ -140,37 +141,36 @@ describeBy(mol[, 2:8], mol$Stage)
 pairs.panels(mol[, 2:8])
 
 ## Create a matrix and run MANOVA----
-m.matrix <- as.matrix(mol[, 2:8])
+m_matrix <- as.matrix(mol[, 2:8])
 
-model.mol <- manova(m.matrix ~ Stage, data = mol)
+model_mol <- manova(m_matrix ~ Stage, data = mol)
 
-model.mol
-summary(model.mol)
+model_mol
+summary(model_mol)
 
-mol.disc <- candisc(model.mol, term = "Stage")
-mol.disc
-summary(mol.disc)
+mol_disc <- candisc(model_mol, term = "Stage")
+mol_disc
+summary(mol_disc)
 
-mol.disc.2 <- candisc(model.mol, data = mol, ndim = 1)
-plot(mol.disc.2, var.col = "black", col = palette,
-var.lwd = 1, ylim = c(-4,5))
+mol_disc2 <- candisc(model_mol, data = mol, ndim = 1)
+plot(mol_disc2, var.col = "black", col = palette, var.lwd = 1, ylim = c(-4, 5))
 
-mol.disc$structure
-mol.disc$coeffs.raw
+mol_disc$structure
+mol_disc$coeffs.raw
 
 # Plot----
-heplot(mol.disc, col = palette, var.col = "black")
-plot(mol.disc,
+heplot(mol_disc, col = palette, var.col = "black")
+plot(mol_disc,
      conf = 0.95, ellipse = TRUE, ellipse.prob = 0.95,
      col = palette, var.col = "black",
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(mol.disc, ellipse = FALSE,
-     col = palette, var.col = "black", 
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(mol.disc, conf = 0.95,
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(mol_disc, ellipse = FALSE,
+     col = palette, var.col = "black",
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(mol_disc, conf = 0.95,
      col = palette, var.col = "black",
      xlim = c(-1, 1), ylim = c(-1, 1),
-     var.lwd = 2, type = 'n')
+     var.lwd = 2, type = "n")
 
 # All Species CDA----
 all_species <- within(data, rm(ID, TL, Depth, "mean.Rug", "ID.chapter", Site))
@@ -192,44 +192,43 @@ describeBy(all_species[, 3:9], all_species$Group)
 pairs.panels(all_species[, 3:9])
 
 ## Create matrix and run MANOVA----
-asp.matrix <- as.matrix(all_species[, 3:9])
+asp_matrix <- as.matrix(all_species[, 3:9])
 
-model.all <- manova(asp.matrix ~ Group, data = all_species)
+model_all <- manova(asp_matrix ~ Group, data = all_species)
 
-model.all
-summary(model.all)
+model_all
+summary(model_all)
 
-all.disc <- candisc(model.all, term = "Group")
-all.disc
-summary(all.disc)
+all_disc <- candisc(model_all, term = "Group")
+all_disc
+summary(all_disc)
 
-all.disc.2 <- candisc(model.all, data = all_species, ndim = 1)
-plot(all.disc.2, var.col = "black", col = palette,
-var.lwd = 1, ylim = c(-4,5))
+all_disc2 <- candisc(model_all, data = all_species, ndim = 1)
+plot(all_disc2, var.col = "black", col = palette, var.lwd = 1, ylim = c(-4, 5))
 
-all.disc$structure
-all.disc$coeffs.std
-all.disc$coeffs.raw
+all_disc$structure
+all_disc$coeffs.std
+all_disc$coeffs.raw
 
 ## Plot----
-n.groups <- nlevels(all_species$Group)
+n_groups <- nlevels(all_species$Group)
 
-palette <- c("#227685","black","#689836","#F2501D",
-             "#849736","#84C5E2","#7D3C98","#F1C40F",
-             "#16A085","#C0392B","#2E86C1","#D35400")[1:n.groups]
-pch.vals <- rep(20, n.groups)
+palette <- c("#227685", "black", "#689836", "#F2501D",
+             "#849736", "#84C5E2", "#7D3C98", "#F1C40F",
+             "#16A085", "#C0392B", "#2E86C1", "#D35400")[1:n_groups]
+pch_vals <- rep(20, n.groups)
 
-heplot(all.disc, col = palette, var.col = "black")
-plot(all.disc,
+heplot(all_disc, col = palette, var.col = "black")
+plot(all_disc,
      conf = 0.95, ellipse = TRUE, ellipse.prob = 0.95,
      col = palette, var.col = "black",
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(all.disc, ellipse = FALSE,
-     col = palette, var.col = "black", 
-     var.lwd = 2, pch = c(20,20,20,20))
-plot(all.disc, conf = 0.95,
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(all_disc, ellipse = FALSE,
+     col = palette, var.col = "black",
+     var.lwd = 2, pch = c(20, 20, 20, 20))
+plot(all_disc, conf = 0.95,
      col = palette, var.col = "black",
      xlim = c(-1, 1), ylim = c(-1.5, 1.5),
-     var.lwd = 2, type = 'n')
+     var.lwd = 2, type = "n")
 
 # End of script----
